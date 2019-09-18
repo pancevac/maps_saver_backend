835df1141883ec6baec9f465861891b43dee4ceb
pancevac <sinisa.buncic7@gmail.com>
2019-09-18 18:32:35 +0200

    Created Route, Point, Track models and migrations

---
+++ database/migrations/2019_09_18_162039_create_routes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoutesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('routes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('trip_id');
+            $table->string('name')->nullable();
+            $table->text('description')->nullable();
+
+            $table->foreign('trip_id')
+                ->references('id')->on('trips')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('routes');
+    }
+}
---
+++ database/migrations/2019_09_18_162155_create_points_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePointsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('points', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->morphs('pointable');
+            $table->decimal('latitude', 10, 8);
+            $table->decimal('longitude', 11, 8);
+            $table->decimal('elevation')->nullable();
+            $table->timestamp('time')->nullable();
+            $table->string('name')->nullable();
+            $table->text('description')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('points');
+    }
+}
